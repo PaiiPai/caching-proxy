de61eb81fe91e8f844cc4ea4097771b869b5f307
paing <paingfamily121@gmail.com>
2025-01-22 22:15:02 +0630

    apply http-proxy-middleware to custom http server

--- server.js
+++ server.js
@@ -1,7 +1,41 @@
 const http = require('http');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const PORT = process.argv[2];
-const URL = process.argv[4];
+const port = process.argv[2];
+const url = new URL(`https://${process.argv[3]}`);
 
-const server = http.createServer().listen(PORT);
+const proxy = createProxyMiddleware({
+  target: url,
+  changeOrigin: true,
+  selfHandleResponse: true,
+});
 
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+  console.log(`Request received for ${req.url}`);
+  let data = '';
+  // Apply proxy middleware to the request
+  proxy(req, res, (proxyRes) => {
+    proxyRes.on('data', (chunk) => {
+      data += chunk;
+    });
+
+    proxyRes.on('end', () => {
+      res.setHeader('X-Cache', 'MISS');
+      res.writeHead('200', proxyRes.getHeaders()).end(data);
+    });
+
+    proxyRes.on('error', (err) => {
+      console.error('Error in proxy server response: ', err);
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('Internal Server Error');
+    });
+  })
+});
+
+server.listen(port, () => {
+  console.log(`
+    Proxy Server initialized at http://localhost:${port}
+    Forwarding requests to ${url}`);
+});
