9a9829084faa11d920415db90042236607107415
paing <paingfamily121@gmail.com>
2025-01-30 09:49:55 +0630

    feat: save cached data in file system and make clear() reset data to {}

--- .cache/cache.js
+++ .cache/cache.js
@@ -1,17 +1,39 @@
+const fs = require('fs');
+
 class Cache {
-	constructor(origin) {
+	constructor(origin, port) {
 		this.origin = origin;
-		this.data = {};
+		this.port = port || 8000;
+		this.clear();
 	}
 
-	save(path, response) {
+	save(key, value) {
 		// Save the value in the cache
-		this.data[path] = response;
+		const cachedData = this.get();
+		if (key && value !== undefined) {
+			cachedData.data[key] = value;
+			fs.writeFile('./data.json', JSON.stringify(cachedData), (err) => err && console.error('Error saving in cache: ', err));
+		} else {
+			console.error('Error saving in cache: Key or value is missing');
+		}
 	}
 
 	get(path) {
 		// Retrieve saved data from the cache
-		return this.data[path];
+		const jsonFile = fs.readFileSync('./data.json', { encoding: 'utf8' });
+		console.log(jsonFile);
+		const jsObj = JSON.parse(jsonFile);
+
+		if (!path) {
+			return jsObj;
+		}
+		return jsObj.data[path];
+	}
+
+	clear() {
+		// Clear all data in this cache instance
+		const initData = { origin: this.origin, port: this.port, data: {} }; // initialize cache data
+		fs.writeFileSync('./data.json', JSON.stringify(initData));
 	}
 }
 
